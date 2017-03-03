f583b3d26aebf3fbdd2746dbea882a98c36adcd2
lostingz <781095823@qq.com>
2017-03-03 18:04:29 +0800

    crud test

--- src/main/java/com/bala/test/controller/CRUDTest.java
+++ src/main/java/com/bala/test/controller/CRUDTest.java
@@ -4,6 +4,8 @@
  */
 package com.bala.test.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,9 +26,11 @@ public class CRUDTest {
     private UserService userService;
 
     @RequestMapping(value = "/add")
-    public void add() {
+    @ResponseBody
+    public String add() {
         // userService.createUser(new User());
-        userService.createUser(new User("asdfasdfasdfasdfasd", "admin", "admin"));
+        userService.createUser(new User(UUID.randomUUID().toString(),"admin", "admin"));
+        return "add success";
     }
 
     @RequestMapping(value = "/get")
